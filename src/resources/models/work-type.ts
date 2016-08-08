087b1e54a6f41fa61c7dfc46982c6bc29df8afd0
Cliffe Hodgkinson <cliffe@resounding.ca>
2016-08-08 11:50:19 -0400

    Add material-only as a work type. Closes #22

--- src/resources/models/work-type.ts
+++ src/resources/models/work-type.ts
@@ -4,9 +4,11 @@ export class WorkType {
 
     static MATERIALS_AND_INSTALL:string = 'm+i';
     static INSTALL_ONLY:string = 'install';
+    static MATERIALS_ONLY:string = 'materials';
 
     static OPTIONS:WorkType[] = [
         { id: WorkType.MATERIALS_AND_INSTALL, name: 'Materials + Install' },
-        { id: WorkType.INSTALL_ONLY, name: 'Install Only' }
+        { id: WorkType.INSTALL_ONLY, name: 'Install Only' },
+        { id: WorkType.INSTALL_ONLY, name: 'Materials Only' }
     ]
 }
