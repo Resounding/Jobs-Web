79d272ffbbfe2958dbe7f5e7a414462b7ec188e5
Cliffe Hodgkinson <cliffe@resounding.ca>
2016-09-08 08:52:10 -0400

    Export to CSV. Closes #37

--- src/resources/views/app.ts
+++ src/resources/views/app.ts
@@ -8,8 +8,8 @@ export class App {
         config.addPipelineStep('authorize', AuthorizeStep);
         config.title = 'Langendoen Mechanical Job Management Application';
         config.map([
-            {route: ['', 'jobs'], name: 'jobs.list', moduleId: 'resources/views/jobs/list', title: 'Jobs List', nav: true, auth: true},
-            {route: 'jobs/new', name: 'jobs.new', moduleId: 'resources/views/jobs/detail', title: 'New Job', nav: true, auth: true},
+            {route: ['', 'jobs'], name: 'jobs.list', moduleId: 'resources/views/jobs/list', title: 'Jobs List', nav: true, auth: true, settings: { icon: 'browser' }},
+            {route: 'jobs/new', name: 'jobs.new', moduleId: 'resources/views/jobs/detail', title: 'New Job', nav: true, auth: true, settings: { icon: 'plus' }},
             {route: 'jobs/:id', name: 'jobs.edit', moduleId: 'resources/views/jobs/detail', title: 'Edit Job', auth: true}
         ]);
 
--- src/resources/views/controls/nav-bar.ts
+++ src/resources/views/controls/nav-bar.ts
@@ -1,27 +1,47 @@
 import {autoinject, bindable} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
 import {Authentication} from '../../services/auth';
+import {CsvExport} from "../../services/csv-export";
+import {Database} from "../../services/data/db";
 
 @autoinject()
 export class NavBar {
 
-    @bindable router:Router;
+  @bindable router: Router;
+  csv: string;
+  changes: PouchCancellable;
 
-    constructor(private element:Element, private auth:Authentication) { }
+  constructor(private element: Element, private auth: Authentication, database: Database, csvExport: CsvExport) {
+    csvExport.export()
+      .then(result => {
+        const csv = encodeURIComponent(result);
+        this.csv = `data:text/csv;charset=utf-8, ${csv}`;
+      });
 
-    attached() {
-        $('.dropdown', this.element).dropdown();
-    }
+    this.changes = database.db.changes({live: true})
+      .on('change', () => {
+        csvExport.export()
+          .then(result => {
+            const csv = encodeURIComponent(result);
+            this.csv = `data:text/csv;charset=utf-8, ${csv}`;
+          });
+      });
+  }
 
-    detached() {
-        $('.dropdown', this.element).dropdown('destroy');
-    }
+  attached() {
+    $('.dropdown', this.element).dropdown();
+  }
 
-    logout() {
-        this.auth.logout();
-    }
+  detached() {
+    $('.dropdown', this.element).dropdown('destroy');
+    this.changes.cancel();
+  }
 
-    get userName() {
-        return (this.auth.userInfo() || { }).name;
-    }
+  logout() {
+    this.auth.logout();
+  }
+
+  get userName() {
+    return (this.auth.userInfo() || {}).name;
+  }
 }
