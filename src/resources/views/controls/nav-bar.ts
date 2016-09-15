ff59d20de0efa1ccb84428af0a34d51eaa854895
Cliffe Hodgkinson <cliffe@resounding.ca>
2016-09-15 06:52:52 -0400

    Generate CSV data on-demand. #41

--- src/resources/views/controls/nav-bar.ts
+++ src/resources/views/controls/nav-bar.ts
@@ -11,22 +11,7 @@ export class NavBar {
   csv: string;
   changes: PouchCancellable;
 
-  constructor(private element: Element, private auth: Authentication, database: Database, csvExport: CsvExport) {
-    csvExport.export()
-      .then(result => {
-        const csv = encodeURIComponent(result);
-        this.csv = `data:text/csv;charset=utf-8, ${csv}`;
-      });
-
-    this.changes = database.db.changes({live: true})
-      .on('change', () => {
-        csvExport.export()
-          .then(result => {
-            const csv = encodeURIComponent(result);
-            this.csv = `data:text/csv;charset=utf-8, ${csv}`;
-          });
-      });
-  }
+  constructor(private element: Element, private auth: Authentication, database: Database, private csvExport: CsvExport) { }
 
   attached() {
     $('.dropdown', this.element).dropdown();
@@ -35,6 +20,19 @@ export class NavBar {
   detached() {
     $('.dropdown', this.element).dropdown('destroy');
     this.changes.cancel();
+  }
+
+  downloadCsv() {
+    this.csvExport.export()
+      .then(result => {
+        const csv = encodeURIComponent(result),
+          href = `data:text/csv;charset=utf-8, ${csv}`,
+          link = document.createElement('a');
+
+        link.download = 'jobs.csv';
+        link.href = href;
+        link.click();
+      });
   }
 
   logout() {
