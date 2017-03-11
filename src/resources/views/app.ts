05f1316679f77051b81a0e1d42e0d077a10925bf
Cliffe Hodgkinson <cliffe@resounding.ca>
2017-03-11 11:25:44 -0500

    Start on mobile calendar

--- src/resources/views/app.ts
+++ src/resources/views/app.ts
@@ -15,11 +15,12 @@ export class App {
         config.addPipelineStep('authorize', AuthorizeStep);
         config.title = 'Langendoen Mechanical Job Management Application';
         config.map([
-            {route: ['', 'jobs'], name: 'jobs.list', moduleId: 'resources/views/jobs/list', title: 'Jobs List', nav: true, auth: true, settings: { icon: 'browser' }},
-            {route: 'jobs/new', name: 'jobs.new', moduleId: 'resources/views/jobs/detail', title: 'New Job', nav: true, auth: true, settings: { icon: 'plus' }},
+            {route: ['', 'jobs'], name: 'jobs.list', moduleId: 'resources/views/jobs/list', title: 'Jobs List', nav: true, auth: true, settings: { icon: 'browser', mobileTitle: 'Jobs' }},
+            {route: 'jobs/new', name: 'jobs.new', moduleId: 'resources/views/jobs/detail', title: 'New Job', nav: true, auth: true, settings: { icon: 'plus', mobileTitle: 'New' }},
             {route: 'jobs/:id', name: 'jobs.edit', moduleId: 'resources/views/jobs/detail', title: 'Edit Job', auth: true},
             {route: 'customers', name: 'customers.list', moduleId: 'resources/views/customers/list', title: 'Customer List', nav: true, auth: true, settings: { icon: 'building outline', hideMobile: true }},
-            {route: 'calendar/:date?', href: '#calendar?:date', name: 'calendar', moduleId: 'resources/views/calendar/calendar', title: 'Calendar', nav: true, auth: true, settings: { icon: 'calendar', hideMobile: true }}
+            {route: 'calendar/:date?', href: '#calendar', name: 'calendar', moduleId: 'resources/views/calendar/calendar', title: 'Calendar', nav: true, auth: true, settings: { icon: 'calendar', hideMobile: true }},
+            {route: 'calendar/mobile/:date?', href: '#calendar/mobile', name: 'calendar.mobile', moduleId: 'resources/views/calendar/mobile', title: 'Calendar', nav: true, auth: true, settings: { icon: 'calendar', hideDesktop: true, mobileTitle: 'Cal' }}
         ]);
 
         this.router = router;
---
+++ src/resources/views/calendar/mobile.ts
@@ -0,0 +1,8 @@
+import {autoinject} from 'aurelia-framework';
+
+@autoinject
+export class Mobile {
+    activate() {
+        console.log('activated');
+    }
+}
