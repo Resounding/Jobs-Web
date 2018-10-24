25471e6f17a7cb3f18305c653d5a7be109b2b746
Cliffe Hodgkinson <cliffe@resounding.ca>
2018-10-24 12:16:22 -0400

    Upgrade deps

--- aurelia_project/tasks/transpile.ts
+++ aurelia_project/tasks/transpile.ts
@@ -1,10 +1,9 @@
 import * as gulp from 'gulp';
 import * as changedInPlace from 'gulp-changed-in-place';
-import * as plumber from 'gulp-plumber';
 import * as sourcemaps from 'gulp-sourcemaps';
-import * as notify from 'gulp-notify';
 import * as rename from 'gulp-rename';
 import * as ts from 'gulp-typescript';
+//@ts-ignore
 import * as project from '../aurelia.json';
 import {CLIOptions, build} from 'aurelia-cli';
 import * as eventStream from 'event-stream';
